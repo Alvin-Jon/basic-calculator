a2f23d646a2ff1341956fd4d3b4fd3a186797486
Alvin-Jon <152227973+Alvin-Jon@users.noreply.github.com>
2024-03-07 18:15:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// accessing the DOM
+let input = document.getElementById('input')
+let output = document.getElementById('answer')
+
+let button = document.querySelectorAll('.btn')
+
+button.forEach((btn,i) => {
+   btn.addEventListener('click', () => {
+    // logic for deleting
+    if(btn.textContent === 'Del'){
+        input.textContent = input.textContent.substring(0, input.textContent.length - 1)
+        evaluate(input.textContent)
+        return
+    }
+    //logic for = :)lol
+    else if(btn.textContent === '='){
+        input.textContent = output.textContent
+        output.textContent = ''
+        return
+    }
+
+    input.textContent += btn.textContent
+    //logic to replace all the x with *
+    let current = input.textContent.replace(new RegExp('x'),'*')
+    evaluate(current)
+   }) 
+})
+
+
+// the evaluating function
+function evaluate(x){
+   let val = x
+   try{
+    let ans =  eval(val)
+    output.textContent = ans 
+   }
+   catch{
+    output.textContent = ' '
+   }
+}
